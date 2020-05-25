e58fd1b4752a076c897bfb13cfe9a59f607fa81c
Satyapriyaarya <satyapriyaarya@gmail.com>
2020-05-25 15:31:59 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using ASPNetCore_DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore_DatingApp.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore_DatingApp.API.Models
+{
+    public class Value 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
